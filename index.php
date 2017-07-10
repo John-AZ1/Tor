00f1dee91d1d7ad4e97a018c1ef8ee3f1bcd102e
John AZ1 <jeremy@exemail.com.au>
2017-07-10 21:19:26 +1000

    Complete Aesthetic
    Including Phone Layout

--- index.php
+++ index.php
@@ -2,13 +2,37 @@
 <html>
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
   <title>Tor</title>
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="assets/fonts/material-icons/material-icons.css">
-</head>
-<body>
-  <h1>Preview Me Now</h1>
+<body style="height: 100%; width:100%; margin: 0px">
+  <!-- Background -->
+    <div id="back-box">
+    </div>
+    <h1>Tor</h1>
+  <!-- Content -->
+    <div id="page">
+      <div class="content">
+        <p>Hello, my name is Jeremy and I programmed this website. It took a while but it works. I just need to add some filler text in order to check the size of every thing. A few lines should do.
+          Question: If I was a millionare would I live to be a 105?
+          Answer: No, because I would be killed for my wealth.</p>
+      </div>
+      <div id="divider"></div>
+      <div class="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
 
+
+  <!-- Nav  -->
+  <nav id="nav">
+    <ul>
+      <li><span><a href="index.php"><i class="material-icons md-light">home</i></a></span></li>
+      <li><i class="material-icons md-light">grade</i></li>
+      <li><i class="material-icons md-light">list</i></li>
+    </ul>
+  </nav>
   <!-- External Scripts -->
     <script src="assets/js/script.js"></script>
 
